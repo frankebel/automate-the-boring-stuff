6b8b0c3a93c56c11672e0a504e9765155407a60c
frankebel <git@ebel.page>
2022-05-31 21:24:33 +0200

    Add exercises from chapter1.

---
+++ chapter1/first-program.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# This program says hello and asks for my name.
+print('Hello World!')
+
+myName =  input('What is your name? ')  # ask for their name
+print(f'It is good to meet you, {myName}.')
+print(f'The length of your name is: {len(myName)}.')
+
+myAge = int(input('What is your age? '))  # ask for their age
+print(f'You will be {myAge + 1} in a year.')
+
